496ef83f1d374ee5b2663c7184d76ab3f057dd2a
baek-un <qordjs700@gmail.com>
2023-03-20 23:39:49 +0900

    OCR

--- app/src/androidTest/java/com/example/wishwash/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/wishwash/ExampleInstrumentedTest.java
@@ -1,7 +1,7 @@
 package com.example.wishwash;
 
 import android.content.Context;
-
+import android.support.test.InstrumentationRegistry;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
